2bc094d7406d5593bf63ec6fa49a2442f8ec16bb
wateri <51120104+mdks777@users.noreply.github.com>
2019-07-30 22:28:27 +0900

    add : hello2
    コピーと完全スライスについて

--- hello/hello2.go
+++ hello/hello2.go
@@ -212,9 +212,9 @@ L:
 	fmt.Println(ss,len(ss),cap(ss))//[0 1 2 3 4] 5 10
 	fmt.Println(ss[2:4])//[2 3]
 	//スライス名[該当数字(無ければ[0])以上：該当数字(なければ[len(変数名)])未満]
-	Ss:=make([]int,5)
-	for i:=5;i<10;i++{Ss[i-5]=i}
-	fmt.Println(append(ss,Ss...)) //[0 1 2 3 4 5 6 7 8 9]
+	Ss:=make([]int,4)
+	for i:=5;i<9;i++{Ss[i-5]=i}
+	fmt.Println(append(ss,Ss...)) //[0 1 2 3 4 5 6 7 8]
 
 	sS:=make([]int,0,0)
 	fmt.Println(len(sS),cap(sS))// 0 0
@@ -226,9 +226,31 @@ L:
 	fmt.Println(len(sS),cap(sS))// 5 8
 	sS=append(sS,6,7,8,9)
 	fmt.Println(len(sS),cap(sS))// 9 16
-	//capは足りなくなったら2のｎ乗になる
+	//capは足りなくなったら2のｎ乗になる,とも限らんらしい
+	SS:=make([]int,1024,1024)
+	fmt.Println(len(SS),cap(SS))//1024 1024
+	SS=append(SS,0)
+	fmt.Println(len(SS),cap(SS))//1025 1344
+	//ちなみに本では1312だった	
 
+	fmt.Println(copy(ss,Ss),ss) //4 [5 6 7 8 4]
+	//変更した個数が値として返還され最初のスライスが変更される
+	//二番目のほうが要素数が多い場合最初のスライスがすべて変更
 	
+	sss:= [10]int {}
+	for i:=0;i<10;i++{sss[i]=i}
+	fmt.Println(sss)
+	ss1:=sss[2:4]
+	fmt.Println(ss1,len(ss1),cap(ss1))//[2 3] 2 8
+	ss2:=sss[2:4:4]
+	fmt.Println(ss2,len(ss2),cap(ss2))//[2 3] 2 2
+	ss3:=sss[2:4:6]
+	fmt.Println(ss3,len(ss3),cap(ss3))//[2 3] 2 4
+	//n:=s[a:b:c]
+	//n==[ s[a]<= X <s[b] ]
+	//len(n)==b-a
+	//cap(n)==c-a
+	//[要素and容量の最小値：要素の最大値：容量の最大値]
 }
 
 
