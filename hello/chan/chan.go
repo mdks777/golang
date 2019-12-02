72f7538ea4b202aa3daa2c45d19b3c8b6367845b
wateri <51120104+mdks777@users.noreply.github.com>
2019-12-02 22:09:39 +0900

    separate
    ついに分けることを覚えた俺はもう無敵や(嘘ですごめんなさい)

--- hello/chan/chan.go
+++ hello/chan/chan.go
@@ -1,45 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	ch := make(chan int, 10)//バッファサイズ10のチャネル、容量みたいなもん（超えたらエラー）
 	//チャネルには「先入れ先出し」「データの順序が保証される」をもつ
 	ch <- 5//チャネルに５を送信
+
 	ch1 := <- ch//チャネルから値を受信
 	fmt.Println(ch1)//5
-		ch2:=make(chan int)
+	
+	ch2:=make(chan int)
 	go receiver(ch2)
 	for i:=0;i<10;i++{
 		ch2<-i
 	}
-		ch3:=make(chan string, 3)
-	ch3<-"APPLE"
-	fmt.Println(len(ch3),cap(ch3))//1 3
-		ch4:= make(chan int, 5)
-	go receive("1st goroutine",ch4)
-	go receive("2nd goroutine",ch4)
-	go receive("3rd goroutine",ch4)
-	for i:=0;i<20;i++{
-		ch <- i
+	fmt.Printf("\n")
+
+	ch4:=make(chan string, 3)
+	ch4<-"APPLE"
+	fmt.Println(len(ch4),cap(ch4))//1 3
+
+	ch5:= make(chan int, 20)
+	go receive("1st goroutine",ch5)//ランダムに
+	go receive("2nd goroutine",ch5)//表示されるため
+	go receive("3rd goroutine",ch5)//毎回表示が違う
+	for i:=0;i<20;i++{ch5 <- i}
+	close(ch5)
+	time.Sleep(3* time.Second)//これないと動かない
+
+	ch6:= make(chan int, 1)
+	ch7:= make(chan int, 1)
+	ch8:= make(chan int, 1)
+	ch6 <- 1
+	ch7 <- 2
+	select {
+	case <-ch6 :
+		fmt.Println("from 1")
+	case <-ch7 :
+		fmt.Println("from 2")
+	case ch8 <- 3:
+		fmt.Println("to 3")
+	default:
+		fmt.Printf("Nothing!")
 	}
-	close(ch4)
 }
 
-func receiver(ch2 <-chan int){//chは送信専用のチャネル
+func receiver(ch2 <-chan int){//ch2は送信専用のチャネル
 	for {
 		i:=<-ch2
-		fmt.Println(i)
+		fmt.Printf("%d",i)
 	}//0123456789
 }
 
-func receive(name string, ch4 <-chan int) {
+func receive(name string, ch5 <-chan int) {
 	for {
-		i,ok:=<-ch4
+		i,ok:=<-ch5
 		if ok == false {
 			break
 		}
-		fmt.Println(i)
+		fmt.Println(name,i)
 	}
 	fmt.Println(name + " is done")
 }
--- hello/hello2.go
+++ hello/hello2.go
@@ -436,27 +436,7 @@ L:
 	fmt.Println(ma2)//map[1:A 3:C 4:D 5:E 6:F]
 	//m:=make(map[int]string,100)->ヒント的な奴。スライスの容量の簡易版的な？
 
-		ch := make(chan int, 10)//バッファサイズ10のチャネル、容量みたいなもん（超えたらエラー）
-	//チャネルには「先入れ先出し」「データの順序が保証される」をもつ
-	ch <- 5//チャネルに５を送信
-	ch1 := <- ch//チャネルから値を受信
-	fmt.Println(ch1)//5
-		ch2:=make(chan int)
-	go receiver(ch2)
-	for i:=0;i<10;i++{
-		ch2<-i
-	}
-		ch3:=make(chan string, 3)
-	ch3<-"APPLE"
-	fmt.Println(len(ch3),cap(ch3))//1 3
-		ch4:= make(chan int, 5)
-	go receive("1st goroutine",ch4)
-	go receive("2nd goroutine",ch4)
-	go receive("3rd goroutine",ch4)
-	for i:=0;i<20;i++{
-		ch <- i
-	}
-	close(ch4)
+	
 }
 
 
